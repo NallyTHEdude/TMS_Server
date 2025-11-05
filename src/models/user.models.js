60da4500bf5f972d8eb12bfcc650d25e6290b14f
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-05 20:08:53 +0530

    no default role assigned

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -11,7 +11,6 @@ const userSchema = new Schema(
             type: String,
             required: true,
             enum: AvailableUserRoles,
-            default: UserRolesEnum.MEMBER
         },
         avatar: {
             type: {
