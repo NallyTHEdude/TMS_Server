7faf312639d0bbf4937b53cb0264266882fa18a9
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-15 19:03:44 +0530

    added property model

---
+++ src/models/property.models.js
@@ -0,0 +1,106 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailablePropertyStatus, PropertyStatusEnum, AvailablePropertyTypes,PropertyTypesEnum} from '../utils/constants.js'; 
+
+
+const propertySchema = new Schema(
+    {
+        status: {
+            type: String,
+            enum: AvailablePropertyStatus,
+            default: PropertyStatusEnum.VACANT,
+            required: true
+        },
+
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description: {
+            type: String,
+            default: null,
+            trim: true
+        },
+
+        issues: {
+            type: [String],
+            default: []
+        },
+
+        country: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        state: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        city: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        pincode: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        address: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        landlordId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true
+        },
+
+        type: {
+            type: String,
+            enum: AvailablePropertyTypes,
+            default: PropertyTypesEnum.FLAT,
+            required: true
+        },
+
+        rentAmount: {
+            type: Number,
+            required: true
+        },
+
+        depositAmount: {
+            type: Number,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Virtuals: tenants and payments linked to the property
+propertySchema.virtual("tenants", {
+    ref: "Tenant",
+    localField: "_id",
+    foreignField: "propertyId"
+});
+
+propertySchema.virtual("payments", {
+    ref: "Payment",
+    localField: "_id",
+    foreignField: "propertyId"
+});
+
+// propertySchema.set("toObject", { virtuals: true });
+// propertySchema.set("toJSON", { virtuals: true });
+
+export default mongoose.model("Property", propertySchema);
