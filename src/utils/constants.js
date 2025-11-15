3cd87222993c1987ddaf080bc31da95761d411bb
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-15 19:04:18 +0530

    added constants related to property

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,13 +3,32 @@ export const UserRolesEnum = {
     PROJECT_ADMIN: 'project_admin',
     MEMBER: 'member'
 }
-
 export const AvailableUserRoles = Object.values(UserRolesEnum);
+
 
 export const TaskStatusEnum = {
     TODO : 'to_do',
     IN_PROGRESS : 'in_progress',
     DONE : 'done'
 }
-
 export const AvailableTaskStatus = Object.values(TaskStatusEnum);
+
+
+//   enum: ["vacant", "occupied", "under-maintenance", "inactive"],
+export const PropertyStatusEnum = {
+    VACANT: 'vacant',
+    OCCUPIED: 'occupied',
+    UNDER_MAINTENANCE: 'under_maintenance',
+    INACTIVE: 'inactive'
+}
+export const AvailablePropertyStatus = Object.values(PropertyStatusEnum);
+
+
+// enum: ["apartment", "villa", "flat", "commercial"],
+export const PropertyTypesEnum = {
+    APARTMENT: 'apartment',
+    VILLA: 'villa',
+    FLAT: 'flat',
+    COMMERCIAL: 'commercial'
+}
+export const AvailablePropertyTypes = Object.values(PropertyTypesEnum);
