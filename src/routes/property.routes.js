d361525d146f361d944c069620447afba884266b
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-16 12:44:36 +0530

    fixed validator and middleware order

--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import {propertyDataValidator, issuesDataValidator} from "../validators/property.validator.js";
+import {requireLandlordRole, requireTenantRole} from "../middlewares/property.middleware.js";
 import {
     addProperty,
     getAllProperties,
@@ -13,11 +14,11 @@ import {
 
 const router = Router();
 
-router.route("/add").post(propertyDataValidator(), verifyJWT, addProperty);
-router.route("/all").get(verifyJWT, getAllProperties);
+router.route("/add").post(verifyJWT, requireLandlordRole, propertyDataValidator(), addProperty);
+router.route("/all").get(verifyJWT, requireLandlordRole, getAllProperties);
 router.route("/:propertyId").get(verifyJWT, getOneProperty);
-router.route("/:propertyId/update").put(propertyDataValidator(), verifyJWT, updateProperty);
-router.route("/:propertyId/add-issues").post(issuesDataValidator(), verifyJWT, addIssuesToProperty);
-router.route("/:propertyId/delete").delete(verifyJWT, deleteProperty);
+router.route("/:propertyId/update").put(verifyJWT, requireLandlordRole, propertyDataValidator(), updateProperty);
+router.route("/:propertyId/add-issues").post(verifyJWT, requireTenantRole, issuesDataValidator(), addIssuesToProperty);
+router.route("/:propertyId/delete").delete(verifyJWT, requireLandlordRole, deleteProperty);
 
 export default router;
