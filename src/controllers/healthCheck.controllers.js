8f3bfa60220aeaa63010d418b89ac78a7c5c215b
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-16 11:06:35 +0530

    removed unnecessar comments

--- src/controllers/healthCheck.controllers.js
+++ src/controllers/healthCheck.controllers.js
@@ -1,18 +1,5 @@
 import { ApiResponse } from '../utils/api-response.js';
 import { asyncHandler } from '../utils/async-handler.js';
-/*
-
-const healthCheck = (req, res) => {
-    try {
-        res
-        .status(200)
-        .json(new ApiResponse(200, {message:'API is healthy'}));
-    } catch (error) {
-        next(error);
-    }
-}
-
-*/
 
 const healthCheck = asyncHandler(async (req, res) => {
     res.status(200).json(
