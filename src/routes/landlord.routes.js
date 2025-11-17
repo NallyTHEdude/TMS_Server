fc15edfc7fbf87983822dd516a4105eeb450ff94
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-17 11:46:03 +0530

    added tenants of a property route

---
+++ src/routes/landlord.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import verifyJWT from "../middlewares/auth.middleware.js";
+import { propertyDataValidator } from "../validators/property.validator.js";
+import { requireLandlordRole } from "../middlewares/property.middleware.js";
+import { getAllProperties } from "../controllers/property.controllers.js";
+import { getAllTenantsOfProperty } from "../controllers/landlord.controllers.js";
+
+const router = Router();
+
+router.route("/properties").get(verifyJWT, requireLandlordRole,propertyDataValidator(), getAllProperties);
+router.route("/tenants/:propertyId").get(verifyJWT, requireLandlordRole, getAllTenantsOfProperty);
+
+export default router;
