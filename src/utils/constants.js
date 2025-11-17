83608f7493851d11694f9f7d1f937a5a19ce301e
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-17 12:07:15 +0530

    added tenant constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -57,3 +57,27 @@ export const LandlordStatusEnum = {
     SUSPENDED: 'suspended'
 }
 export const AvailableLandlordStatus = Object.values(LandlordStatusEnum);
+
+
+
+// TENANT ENUMS
+export const TenantStatusEnum = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive',
+    EVICTED: 'evicted'
+}
+export const AvailableTenantStatus = Object.values(TenantStatusEnum);
+
+export const KycStatusEnum = {
+    PENDING: 'pending',
+    VERIFIED: 'verified',
+    REJECTED: 'rejected'
+}
+export const AvailableKycStatus = Object.values(KycStatusEnum);
+
+export const TenantPaymentStatusEnum = {
+    PAID: 'paid',
+    DUE: 'due',
+    OVERDUE: 'overdue'
+}
+export const AvailableTenantPaymentStatus = Object.values(TenantPaymentStatusEnum);
