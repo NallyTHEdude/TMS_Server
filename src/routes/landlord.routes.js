c998ea33284e9dcf6980af1aee2688cbeeac6cac
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-11-17 17:42:39 +0530

    all landlord routes work good as many i have done

--- src/routes/landlord.routes.js
+++ src/routes/landlord.routes.js
@@ -3,11 +3,11 @@ import verifyJWT from "../middlewares/auth.middleware.js";
 import { propertyDataValidator } from "../validators/property.validator.js";
 import { requireLandlordRole } from "../middlewares/property.middleware.js";
 import { getAllProperties } from "../controllers/property.controllers.js";
-import { getAllTenantsOfProperty } from "../controllers/landlord.controllers.js";
+import { getAllActiveTenantsOfProperty } from "../controllers/landlord.controllers.js";
 
 const router = Router();
 
 router.route("/properties").get(verifyJWT, requireLandlordRole,propertyDataValidator(), getAllProperties);
-router.route("/tenants/:propertyId").get(verifyJWT, requireLandlordRole, getAllTenantsOfProperty);
+router.route("/tenants/:propertyId").get(verifyJWT, requireLandlordRole, getAllActiveTenantsOfProperty);
 
 export default router;
