6f22ddf3346b533498c25f1d8d5b24ecb9e1a555
NallyTHEdude <rifaqatnawaz25@gmail.com>
2025-12-16 19:18:03 +0530

    added filterProperties route, but testing to be done yet

--- src/routes/landlord.routes.js
+++ src/routes/landlord.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import verifyJWT from '../middlewares/auth.middleware.js';
-import { propertyDataValidator } from '../validators/property.validator.js';
+import { propertyDataValidator, filterPropertiesValidator } from '../validators/property.validator.js';
 import { requireLandlordRole } from '../middlewares/property.middleware.js';
+import { validate } from '../middlewares/validator.middleware.js';
 import { getAllProperties } from '../controllers/property.controllers.js';
-import { getAllActiveTenantsOfProperty } from '../controllers/landlord.controllers.js';
+import { getAllActiveTenantsOfProperty, filterProperties } from '../controllers/landlord.controllers.js';
 
 const router = Router();
 
@@ -15,6 +16,17 @@ router
         propertyDataValidator(),
         getAllProperties,
     );
+
+router
+    .route('/properties/filter')
+    .get(
+        verifyJWT,
+        requireLandlordRole,
+        filterPropertiesValidator(),
+        validate,
+        filterProperties
+    );
+
 router
     .route('/tenants/:propertyId')
     .get(verifyJWT, requireLandlordRole, getAllActiveTenantsOfProperty);
