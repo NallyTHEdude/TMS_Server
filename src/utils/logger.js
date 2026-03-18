7fe3789f7f384a5399b8e0fb1a2972015511898e
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-03-18 10:40:47 +0530

    setup logger instance

---
+++ src/utils/logger.js
@@ -0,0 +1,36 @@
+import winston from "winston";
+import { config } from "../config/index.js";
+
+const isDevEnv = config.NODE_ENV === "development"; // can only be develpment or production
+
+// defining log format for dev and prod environment
+const devFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.errors({ stack: true }),
+  winston.format.splat(),
+  winston.format.printf(({ level, message, timestamp, stack, ...meta }) => {
+    const metaString = Object.keys(meta).length
+        ? JSON.stringify(meta, null, 2)
+        : "";
+
+    return `${timestamp} [${level}]: ${stack || message} ${metaString}`;
+ })
+);
+
+const prodFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.errors({ stack: true }),
+  winston.format.splat(),
+  winston.format.json()
+);
+
+// creating logger instance
+export const logger = winston.createLogger({
+  level: "info",
+  format: isDevEnv ? devFormat : prodFormat,
+  transports: [
+    new winston.transports.Console(),
+    !isDevEnv ? new winston.transports.File({ filename: "logs/app.log" }) : null
+  ]
+});
