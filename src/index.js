5292bf432c948072cc9fc275eb9eba867fe852b0
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-03-18 11:03:23 +0530

    removed unnecessary .then and .catch

--- src/index.js
+++ src/index.js
@@ -6,14 +6,7 @@ import { logger } from './utils/logger.js';
 const PORT = config.PORT;
 const BASE_URL = config.BASE_URL;
 
-connectDB()
-    .then(() => {
-        console.log(`Mongodb is perfectly connected`);
-    })
-    .catch((err) => {
-        console.error('Failed to connect to the database:', err);
-        process.exit(1);
-    });
+await connectDB()
 
 //app listener
 app.listen(PORT, () => {
