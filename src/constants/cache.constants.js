d9ff6c28f6872ba4ae150f515c0afdd765317270
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-03-24 09:35:17 +0530

    updated with better keys handling

--- src/constants/cache.constants.js
+++ src/constants/cache.constants.js
@@ -1,10 +1,13 @@
-export const CachePrefixes = {
-    TENANT: 'tenant',
-    LANDLORD: 'landlord',
-    PROPERTY: 'property',
-}
-export const CacheKeys = {
-    TENANT: 'tenant',
-    LANDLORD: 'landlord',
-    PROPERTY: 'property',
+export const cacheKeys = {
+    TENANT: (id) => `tenant:${id}`,
+    LANDLORD: (id) => `landlord:${id}`,
+    PROPERTY: (id) => `property:${id}`,
 };
+export const cacheKeysEnum  = Object.values(cacheKeys);
+
+export const cacheTTL = {
+    TENANT_TTL: 3600, // 1 hour
+    LANDLORD_TTL: 3600, // 1 hour
+    PROPERTY_TTL: 3600, // 1 hour
+};
+export const cacheTTLEnum = Object.values(cacheTTL);
