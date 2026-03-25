008200c7c5fb2167fdfe5f52e7c1b827eec85f02
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-03-25 10:35:40 +0530

    updated cache constants

--- src/constants/cache.constants.js
+++ src/constants/cache.constants.js
@@ -1,13 +1,25 @@
-export const cacheKeys = {
-    TENANT: (id) => `tenant:${id}`,
-    LANDLORD: (id) => `landlord:${id}`,
-    PROPERTY: (id) => `property:${id}`,
+export const CacheEntities = {
+    TENANT: `tenant`,
+    LANDLORD: `landlord`,
+    PROPERTY: `property`,
 };
-export const cacheKeysEnum  = Object.values(cacheKeys);
+export const CacheEntitiesEnum  = Object.values(CacheEntities);
 
-export const cacheTTL = {
+export const CacheTTL = {
     TENANT_TTL: 3600, // 1 hour
     LANDLORD_TTL: 3600, // 1 hour
     PROPERTY_TTL: 3600, // 1 hour
 };
-export const cacheTTLEnum = Object.values(cacheTTL);
+export const CacheTTLEnum = Object.values(CacheTTL);
+
+export const CacheIdentifiers = {
+    KYC:(id)=> `kyc:${id}`,
+    GET_ALL_PROPERTIES:(id)=> `getAllProperties:${id}`,
+    GET_ONE_PROPERTY: (id)=> `getOneProperty:${id}`,
+    GET_ALL_TENANTS: (id)=> `getAllTenants:${id}`,
+    GET_ONE_TENANT: (id)=> `getOneTenant:${id}`,
+    GET_ALL_LANDLORDS: (id)=> `getAllLandlords:${id}`,
+    GET_ONE_LANDLORD: (id)=> `getOneLandlord:${id}`,
+    GET_ACTIVE_TENANTS_BY_PROPERTY: (id)=> `getActiveTenantsByProperty:${id}`,
+}
+export const CacheIdentifiersEnum = Object.values(CacheIdentifiers);
